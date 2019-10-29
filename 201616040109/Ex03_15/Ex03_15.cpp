49d6b2f3d9efc3fc772c36a96d041eede0ffb6a3
软件1601 李坤 <51869823+Kun-Gustaf@users.noreply.github.com>
2019-10-29 19:58:38 +0800

    Create Ex03_15.cpp
    This is my assignment_03
    Student ID:201616040109

---
+++ 201616040109/Ex03_15/Ex03_15.cpp
@@ -0,0 +1,15 @@
+// Exercise 3.15 Solution: Ex03_15.cpp
+// Test program for modified Date class.
+#include <iostream>
+
+#include "Date.h"// include definitions of class Date
+
+using namespace std;
+int main()
+{
+
+    Date date(13,12,2012);// instantiate object d of class Date
+
+    //output Date object date's value
+    date.displayDate();
+}
