739046c1f5dfd47748db6956d0e8469dfd8d1990
201714040201 <56496933+201714040201@users.noreply.github.com>
2019-10-30 17:21:38 +0800

    Create Ex03_15.cpp
    this is my assignment
    student ID:201714040201

---
+++ 201714040201/Ex03_15/Ex03_15.cpp
@@ -0,0 +1,18 @@
+//Ex03_15.cpp
+#include <iostream>
+#include <string>
+#include "Date.h"
+
+using namespace std;
+
+int main()
+{
+    //initialize object date
+    Date date1(2,28,1999);
+    //initialize object date
+    Date date2(4,28,1999);
+     //output date's message
+    date1.disPlay();
+    //output date's message
+    date2.disPlay();
+}//end main
