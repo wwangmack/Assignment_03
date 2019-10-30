6b3322594a28acdc09f2b4b81238f87941f498fb
hhhhhepc <45264381+hhhhhpc@users.noreply.github.com>
2019-10-30 17:47:21 +0800

    Update Date.h

--- 201816040309/Ex03_15/Date.h
+++ 201816040309/Ex03_15/Date.h
@@ -1,1 +1,17 @@
 
+class Date
+{
+public:
+   Date( int,int,int ); // constructor initializes balance
+    void set_year(int);
+    void set_month(int);
+    void set_day(int);
+    void displayDate();
+    int get_year();
+    int get_month();
+    int get_day();
+private:
+   int month;
+   int year;
+   int day;
+};
