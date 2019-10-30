8b2a8f5c7aa81f6219b9b3dab5cc4f55d9993780
hhhhhepc <45264381+hhhhhpc@users.noreply.github.com>
2019-10-30 16:48:43 +0800

    Update invoice.h

--- 201816040309/Ex03_13/invoice.h
+++ 201816040309/Ex03_13/invoice.h
@@ -1,1 +1,21 @@
 
+class Invoice
+{
+public:
+   Invoice( string,string,int,int ); // constructor initializes balance
+    void set_product_number(string);
+    void set_product_describe(string);
+    void set_product_count(int);
+    void set_product_price(int);
+    string get_product_number();
+    string get_product_describe();
+    int get_product_count();
+    int get_product_price();
+    int getInvoiceAmount();
+private:
+   string product_number;
+   string product_describe;
+   int product_count;
+   int product_price;
+
+};
