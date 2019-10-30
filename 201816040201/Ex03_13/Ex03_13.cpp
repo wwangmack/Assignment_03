9dec053a80984870e2342ea981bd6782e70f743d
huatzy <56495508+huatzy@users.noreply.github.com>
2019-10-30 22:17:24 +0800

    Update Ex03_13.cpp

--- 201816040201/Ex03_13/Ex03_13.cpp
+++ 201816040201/Ex03_13/Ex03_13.cpp
@@ -6,11 +6,11 @@ using namespace std;
 int main()
 {
     printf("零件");
-     Invoice getone("007","螺丝",50,1);
+     Invoice getone("007","螺丝",-1,-1);///非法定义价格和数量
      cout<<"零件信息："<<endl;
      cout<<"零件号： "<<getone.getlingjianhao()<<" 零件名： "<<getone.getlingjianname()<<" 零件数量： "<<getone.getnumber()<<" 零件单价： "<<getone.getmoney()<<endl;
-     getone.setnumber(60);
-     getone.setmoney(2);
+     getone.setnumber(60);///修改数量
+     getone.setmoney(2);///修改价格
       cout<<"零件修改信息："<<endl;
      cout<<"零件号： "<<getone.getlingjianhao()<<" 零件名： "<<getone.getlingjianname()<<" 零件数量： "<<getone.getnumber()<<" 零件单价： "<<getone.getmoney()<<endl;
      cout<<"总额:"<<getone.getinvoiceamount(60,2);
