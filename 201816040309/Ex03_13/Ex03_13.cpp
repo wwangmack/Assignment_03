20196c47e420b3dd28979c23d93f7343c4a85fa3
hhhhhepc <45264381+hhhhhpc@users.noreply.github.com>
2019-10-30 16:48:13 +0800

    Update Ex03_13.cpp

--- 201816040309/Ex03_13/Ex03_13.cpp
+++ 201816040309/Ex03_13/Ex03_13.cpp
@@ -1,1 +1,15 @@
 
+#include <iostream>
+using namespace std;
+
+#include "invoice.h"
+
+int main()
+{
+    Invoice product1("0001","this is a good product",10,20);
+    cout<<product1.get_product_count()<<endl;
+    cout<<product1.get_product_describe()<<endl;
+    cout<<product1.get_product_number()<<endl;
+    cout<<product1.get_product_price()<<endl;
+    cout<<product1.getInvoiceAmount()<<endl;
+}
