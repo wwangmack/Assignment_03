fbd331b24e0cda5052506931742c712ae98dbc40
201816040121 <56394242+201816040121@users.noreply.github.com>
2019-10-31 23:01:27 +0800

    Create Ex03_15.cpp

---
+++ 201816040121/Ex03_15/Ex03_15.cpp
@@ -0,0 +1,15 @@
+//Ex03_15:DateTest.cpp
+#include <iostream>
+#include "Date.h"
+using namespace std;
+
+//fuction main begins program execution
+int main()
+{
+    //Creative a Date object
+    Date date(2019, 10, 31);
+    /*Display the date message*/
+    date.displayDate();
+
+    return 0;
+}
