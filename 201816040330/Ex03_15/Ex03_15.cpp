09ad721c24f7c66cda261a01a0caf69d31c72939
201816040330 <56644045+201816040330@users.noreply.github.com>
2019-10-31 23:32:04 +0800

    Create Ex03_15.cpp

---
+++ 201816040330/Ex03_15/Ex03_15.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Date.h"//include definition of class Date
+
+using namespace std;
+
+int main()
+{
+    Date ddate(0,0,0);//chushihua
+    ddate.displayDate(2018,8,9);
+
+    return 0;
+}
