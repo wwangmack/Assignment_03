8d7ad914ce81bb706c10fdda956b5c65292daaf4
zyyytt <54845294+zyyytt@users.noreply.github.com>
2019-10-31 21:27:35 +0800

    Create Ex03_13 and Ex03_15

---
+++ 201816040326/Ex03_13/Ex03_13.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include"Invoice.h"
+#include<string>
+using namespace std;
+
+int main()
+{
+    Invoice component1("00001","Halloween candy",100,1);
+    Invoice component2("00002","Pumpkin Lantern",20,15);
+    cout<<"This is your components:"<<endl<<endl;
+    cout<<component1.getcomponent_ID()<<" "<<component1.getcomponent_description()<<" "<<component1.getcomponent_salesvolume()<<" "<<component1.getcomponent_price()<<" "<<"amount:"<<component1.getInvoiceAmount()<<endl;
+    cout<<component2.getcomponent_ID()<<" "<<component2.getcomponent_description()<<" "<<component2.getcomponent_salesvolume()<<" "<<component2.getcomponent_price()<<" "<<"amount:"<<component2.getInvoiceAmount()<<endl;
+    return 0;
+}
