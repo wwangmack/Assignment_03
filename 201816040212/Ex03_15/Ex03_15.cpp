f22a950a586f2b76e0d5221226b2b32c83eb5062
twilight <56493513+TwilightNougat@users.noreply.github.com>
2019-10-31 18:35:02 +0800

    Create Ex03_15.cpp

---
+++ 201816040212/Ex03_15/Ex03_15.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ass3
+//
+//  Created by MacBook on 2019/10/28.
+//  Copyright © 2019 MacBook. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+#include "Invoice.h"
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    Invoice myin(2019,10,12);
+    myin.displayDate();
+}
